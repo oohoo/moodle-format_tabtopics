22f71c41fbe374b29318e31a2df5d1744ab7192b
Dustin Durand <dddurand@ualberta.ca>
2013-06-18 08:04:32 -0600

    Incorrect date for required moodle version fixed.
    -Now corresponds to moodle 2.5 stable release

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013061700;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2012051400;        // Requires this Moodle version.
+$plugin->requires = 2013051400;        // Requires this Moodle version.
 $plugin->component = 'format_tabtopics';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.5.1 (Build: 2013061700)';
